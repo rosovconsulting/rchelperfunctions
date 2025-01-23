ffbf39b373e5bdde1e07a962155f581694df39c4
Megan Brown <mbrown@rosovconsulting.com>
2025-01-23 14:05:05 -0800

    add multi_response_table() and multi_response_table_style()

---
+++ R/multi_response_table.R
@@ -0,0 +1,32 @@
+#' Function for Multiple-response question frequency table
+#'
+#' @param data Survey data with multireponse split into multiple columns
+#' @param ... expss selection 
+#'
+#' @return Multi-response dataframe
+#' @export
+#' 
+#' @examples
+#' multi_response_table(data = survey_data, mdset_t("q1_2_{1:11}")) %>% 
+#' multi_response_table_style()
+#' 
+#' 
+multi_response_table = function(data, ...) {
+  data %>% 
+    expss::tab_cells(...) %>%
+    expss::tab_stat_cases(label = "n",
+                   total_row_position = "below") %>% # Adds n columns
+    expss::tab_stat_cpct(label = "percent of cases",
+                  total_row_position = "none") %>% # adds percent of cases column
+    expss::tab_pivot(stat_position = "inside_columns") %>% 
+    expss::tab_sort_desc(excluded_rows = c("Other", "#")) %>% # sort without including rows with # and Other
+    expss::split_table_to_df() %>% # transform to dataframe so you can use gt()
+    janitor::row_to_names(row_number = 2, remove_row = TRUE, remove_rows_above = TRUE) %>% 
+    dplyr::rename("q" = 1) %>% 
+    dplyr::mutate(`percent of cases` = as.numeric(`percent of cases`)) %>% 
+    dplyr::mutate(`percent of cases` = `percent of cases`/100) %>% 
+    dplyr::filter(!grepl('- Text', q)) %>% #Remove row for Other - Text
+    dplyr::mutate(q = str_replace_all(q, stringr::fixed("#"), "")) # remove # from Total row
+    
+  }
+  
---
+++ R/multi_response_table_style.R
@@ -0,0 +1,36 @@
+#' Function for styling Multiple-response question frequency table in RC branding 
+#'
+#' @param x result of multi_response_table
+#'
+#' @return Styled multi-response dataframe
+#' @export
+#' 
+#' @examples
+#' multi_response_table(data = survey_data, mdset_t("q1_2_{1:11}")) %>% 
+#' multi_response_table_style()
+#' 
+#' 
+multi_response_table = function(x) {
+  x %>% 
+    gt::gt() %>% 
+    gt::sub_missing(columns = "percent of cases",
+                  missing_text = "-") %>% 
+    gt::fmt_percent(columns = `percent of cases`,
+                      decimals = 0) %>%
+    gt::cols_label(q = "") %>% 
+    gt::tab_style(style = list(cell_text(color = "white",
+                                       weight = "bold"),
+                                       gt::cell_fill(color = "#5C068C")),
+                locations = cells_title("title")) %>% 
+    gt::tab_style(style = gt::cell_borders(sides = c("top"),
+                                     weight = px(2),
+                                     style = "solid",
+                                     color = "#D3D3D3"),
+                  locations = gt::cells_body(columns = tidyselect::everything(),
+                                             rows = tidyselect::last_col())) %>% 
+    gt::opt_table_lines() %>% 
+    gt::tab_options(table.align = "left",
+                  data_row.padding = px(5)
+                  )
+  }
+  
