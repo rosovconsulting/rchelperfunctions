745cba914a8ff275dbbaf2cee858f5479e7a8cd0
Megan Brown <mbrown@rosovconsulting.com>
2025-05-15 16:19:43 -0700

    add roxygen comment to set_dropbox_path()

--- R/set_dropbox_path.R
+++ R/set_dropbox_path.R
@@ -1,10 +1,14 @@
+#' Set path to Dropbox folder to DROPBOX_PATH
+#'
+#' @export
+#' 
 set_dropbox_path <- function() {
   dropbox_folder <- function() {
     if (Sys.info()["sysname"] != "Windows") {
       warning("Currently, 'dropbox_folder' works for Windows only. Sorry.")
       return(NULL)
     }
-    
+
     file_name <- list.files(
       path = paste(Sys.getenv(x = "APPDATA"), "Dropbox", sep = "/"),
       pattern = "info*.json",
@@ -17,17 +21,17 @@ set_dropbox_path <- function() {
         full.names = TRUE
       )
     }
-    
+
     if (length(file_name) == 0) {
       warning("No Dropbox info file found.")
       return(NULL)
     }
-    
+
     file_content <- jsonlite::fromJSON(txt = file_name)$business
     path <- paste0(file_content$root_path, "/")
     path
   }
-  
+
   dropbox_path <- dropbox_folder()
   if (!is.null(dropbox_path)) {
     Sys.setenv(DROPBOX_PATH = dropbox_path)
