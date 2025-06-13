2c49fd11ce7e41438fb681b9c35050aa0a788ff3
Megan Brown <mbrown@rosovconsulting.com>
2025-06-13 15:17:02 -0700

    format likert_table_style.R

--- R/likert_table_style.R
+++ R/likert_table_style.R
@@ -5,26 +5,30 @@
 #'
 #' @return Styled gt table
 #' @export
-#' 
+#'
 likert_table_style = function(x, table_title) {
-  
-  x %>% 
-    dplyr::left_join(., merged_col_map) %>% 
-      dplyr::select(-Item) %>% 
-        dplyr::select(Label, tidyselect::everything()) %>% 
-          dplyr::rename(Item = Label) %>% 
-            dplyr::filter(dplyr::if_any(tidyselect::where(is.numeric) & !tidyselect::matches("^N$"), ~.x != 0)) %>% 
-    gt::gt(rowname_col = "Group",
-           groupname_col = "Item") %>% 
-    gt::fmt_percent(columns = tidyselect::where(is.numeric) & !tidyselect::matches("^N$"),
-                decimals = 0) %>% 
-                  gt::tab_header(table_title) %>% 
-    gt::tab_style(style = list(gt::cell_text(color = "white",
-                                       weight = "bold"),
-                                       gt::cell_fill(color = "#5C068C")),
-                locations = gt::cells_title("title")) %>%
-    gt::tab_options(table.align = "left",
-                row_group.as_column = TRUE
-    ) %>% 
+  x %>%
+    dplyr::left_join(., merged_col_map) %>%
+    dplyr::select(-Item) %>%
+    dplyr::select(Label, tidyselect::everything()) %>%
+    dplyr::rename(Item = Label) %>%
+    dplyr::filter(dplyr::if_any(
+      tidyselect::where(is.numeric) & !tidyselect::matches("^N$"),
+      ~ .x != 0
+    )) %>%
+    gt::gt(rowname_col = "Group", groupname_col = "Item") %>%
+    gt::fmt_percent(
+      columns = tidyselect::where(is.numeric) & !tidyselect::matches("^N$"),
+      decimals = 0
+    ) %>%
+    gt::tab_header(table_title) %>%
+    gt::tab_style(
+      style = list(
+        gt::cell_text(color = "white", weight = "bold"),
+        gt::cell_fill(color = "#5C068C")
+      ),
+      locations = gt::cells_title("title")
+    ) %>%
+    gt::tab_options(table.align = "left", row_group.as_column = TRUE) %>%
     gt::opt_table_lines()
 }
