85df15ea0070a0d9948ea584c9fff19ee2480440
Megan Brown <mbrown@rosovconsulting.com>
2025-08-19 16:09:23 -0700

    switch multi_response_table.R to native pipe

--- R/multi_response_table.R
+++ R/multi_response_table.R
@@ -8,62 +8,62 @@
 #' @export
 #'
 #' @examples
-#' \dontrun{multi_response_table(data = survey_data, mdset_t("q1_2_{1:11}")) %>%
+#' \dontrun{multi_response_table(data = survey_data, mdset_t("q1_2_{1:11}")) |> 
 #'   multi_response_table_style()}
 #'
 #'
 multi_response_table = function(data, ..., cross = NULL) {
   if (missing(cross)) {
-    x <- data %>%
-      expss::tab_cells(...) %>%
-      expss::tab_stat_cases(label = "n", total_row_position = "below") %>% # Adds n columns
+    x <- data |> 
+      expss::tab_cells(...) |> 
+      expss::tab_stat_cases(label = "n", total_row_position = "below") |> # Adds n columns
       expss::tab_stat_cpct(
         label = "percent of cases",
         total_row_position = "none"
-      ) %>% # adds percent of cases column
-      expss::tab_pivot(stat_position = "inside_columns") %>%
-      expss::tab_sort_desc(excluded_rows = c("Other", "#", ":", "None of the above", "Prefer not to answer")) %>% # sort without including rows with #, a colon, and Other
+      ) |> # adds percent of cases column
+      expss::tab_pivot(stat_position = "inside_columns") |>
+      expss::tab_sort_desc(excluded_rows = c("Other", "#", ":", "None of the above", "Prefer not to answer")) |> # sort without including rows with #, a colon, and Other
       expss::split_table_to_df(remove_repeated = FALSE)
   } else {
     x <- eval(substitute({
-      data %>%
-        expss::tab_cells(...) %>%
-        expss::tab_cols(cross) %>%
-        expss::tab_stat_cases(label = "n", total_row_position = "below") %>% # Adds n columns
+      data |>
+        expss::tab_cells(...) |>
+        expss::tab_cols(cross) |>
+        expss::tab_stat_cases(label = "n", total_row_position = "below") |> # Adds n columns
         expss::tab_stat_cpct(
           label = "percent of cases",
           total_row_position = "none"
-        ) %>% # adds percent of cases column
-        expss::tab_pivot(stat_position = "inside_columns") %>%
-        expss::tab_sort_desc(excluded_rows = c("Other", "#", ":", "None of the above", "Prefer not to answer")) %>% # sort without including rows with #, a colon, and Other
+        ) |> # adds percent of cases column
+        expss::tab_pivot(stat_position = "inside_columns") |>
+        expss::tab_sort_desc(excluded_rows = c("Other", "#", ":", "None of the above", "Prefer not to answer")) |> # sort without including rows with #, a colon, and Other
         expss::split_table_to_df(remove_repeated = FALSE)
     }))
   }
 
   if (!missing(cross)) {
-    x <- x %>%
-      stats::setNames(paste(x[2, ], x[3, ], sep = "_")) %>%
-      dplyr::rename("q" = 1) %>%
+    x <- x |>
+      stats::setNames(paste(x[2, ], x[3, ], sep = "_")) |>
+      dplyr::rename("q" = 1) |>
       filter(q != "")
   } else {
-    x <- x %>%
+    x <- x |>
       janitor::row_to_names(
         row_number = 2,
         remove_row = TRUE,
         remove_rows_above = TRUE
-      ) %>%
+      ) |>
       dplyr::rename("q" = 1)
   }
 
-  x %>%
+  x |>
     dplyr::mutate(across(
       tidyselect::matches("percent of cases"),
       ~ as.numeric(.x)
-    )) %>%
+    )) |>
     dplyr::mutate(across(
       tidyselect::matches("percent of cases"),
       ~ .x / 100
-    )) %>%
-    dplyr::filter(!grepl('- Text', q)) %>% #Remove row for Other - Text
+    )) |>
+    dplyr::filter(!grepl('- Text', q)) |> #Remove row for Other - Text
     dplyr::mutate(q = stringr::str_replace_all(q, stringr::fixed("#"), "")) # remove # from Total row
 }
