0a8481a574c61d5ae43c94f7df18335c47a521e0
Megan Brown <mbrown@rosovconsulting.com>
2026-02-13 13:35:22 -0800

    add documentation for create_column_map

--- R/create_column_map.R
+++ R/create_column_map.R
@@ -1,8 +1,12 @@
-#' Create column map
+#' Create a column map for use as a codebook. Functions like `likert_table_style` use a column map to add variable labels to a table.
 #'
 #' @param data A data frame with variable labels
-#' 
-#' @return A dataframe
+#'
+#' @return A dataframe with question numbers and labels. Columns in the output data frame are:
+#'  \describe{
+#'    \item{Item}{variable name}
+#'    \item{Label}{variable label}
+#'    }
 #' @export
 #'
 #' @examples
@@ -16,6 +20,6 @@ create_column_map = function(data) {
       cols = tidyselect::everything(),
       names_to = "Item",
       values_to = "Label"
-    ) |> 
+    ) |>
     dplyr::mutate(Label = gsub(pattern = '.*. - ', "", Label))
 }
